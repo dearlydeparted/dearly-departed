3941703d2dfbb6f7672764172394055faeaa870b
pabblloo <dearlydeparted.io@gmail.com>
2022-08-19 20:19:50 -0400

    nwq
    new

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
+import React from "react";
+import {
+  AppBar,
+  Container,
+} from "@material-ui/core";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,Switch,Link
+} from "react-router-dom";
+import "./App.css";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import Home from "./Pages/home";
+import About from "./Pages/about";
 
-import './App.css';
-
+//import Dashboard from "./components/Dashboard/Dashboard";
+  
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-       <h1> DEARLY DEPARTED</h1>
+      <Router>
+     
+        <Container maxWidth="lg" className="container">
+        <span className="intro">Welcome to:</span>
+         <h1> DEARLY DEPARTED</h1>
+         <p>Transparency pricing and expert help in Age and Health transitions</p>
+
+        </Container>
   
-      </header>
+      </Router>
     </div>
   );
 }
-
+  
 export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () =>{
+
+return(
+
+
+
+<h5> footer</h5>
+
+
+
+	);
+
+
+
+
+
+
+
+};
+export default Footer;
